d6d790552ff7c0a3a694a8202b54d179534ffafa
rusnnee <36538819+rusnnee@users.noreply.github.com>
2026-02-25 15:54:56 +0000

    Add solution to 14-fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let current = 1; 
+    let past = 0; 
 
+    if (num < 0) return "OOPS"; 
+    if (num == 0) return 0; 
+    if (num == 1) return 1
+    else {
+        for (let i = 2; i <=num; i++){
+            let temp = current;
+            current = past + current; 
+            past = temp; 
+        }
+        return current; 
+    }
 };
 
 // Do not edit below this line
