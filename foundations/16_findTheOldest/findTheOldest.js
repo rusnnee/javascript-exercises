aea5408a47359048bd245322778c4a9410a20581
rusnnee <36538819+rusnnee@users.noreply.github.com>
2026-02-25 16:10:48 +0000

    Add solution to 16-findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+const getAge = function (person){
+    if (person.yearOfDeath == undefined) return new Date().getFullYear()  - person.yearOfBirth;
+    return person.yearOfDeath - person.yearOfBirth
+}
 
+const findTheOldest = function(people) {
+    let sorted = people.sort((a, b) => (getAge(b))- (getAge(a)))
+    return sorted[0]; 
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
