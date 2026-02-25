9a867c28bb64b839def7469bf839039af2343469
rusnnee <36538819+rusnnee@users.noreply.github.com>
2026-02-25 15:33:01 +0000

    Add solution to 12-calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((acc, num) => acc + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+	return nums.reduce((acc, num) => acc * num, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  if (exp == 0) return 1
+  else{
+    let num = base; 
+    for (let i = 2; i <= exp; i++){
+      num = num * base; 
+    }
+  return num; 
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  for (let i = 1; i <= num; i++){
+    fact = fact * i; 
+  }
+  return fact;
 };
 
 // Do not edit below this line
