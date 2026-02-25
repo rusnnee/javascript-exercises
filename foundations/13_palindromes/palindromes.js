4f6014f61a1ca12b235b1652e170ebdda11a5738
rusnnee <36538819+rusnnee@users.noreply.github.com>
2026-02-25 15:52:10 +0000

    Add solution to 13-palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/\W|_/g, '');
+    str = str.toLowerCase(); 
+    console.log(str); 
+    return str === str.split("").reverse().join(""); 
 };
 
 // Do not edit below this line
